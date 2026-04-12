07f8be31c324fa997127ad108b7eb484824e0d7d
rseleznev <gradoozd@gmail.com>
2026-04-12 15:29:25 +0300

    Удалил файлы, которые уже перенес в другие модули

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -1,111 +1,36 @@
 package connection
 
 import (
-	"errors"
-	"syscall"
+	"sync"
 
-	"github.com/rseleznev/redis_driver/pkg/polling"
 	"github.com/rseleznev/redis_driver/internal/models"
-	"github.com/rseleznev/redis_driver/internal/connection/socket"
 )
 
-// New создает новое подключение
-func New(opts models.Options) (int, error) {
-	// Создаем сокет
-	socketFd, err := socket.New(opts.TCPSendBufLen, opts.TCPReceiveBufLen)
-	if err != nil {
-		return 0, err
-	}
-
-	// Ставим на отслеживание первичное событие
-	err = polling.InitEventForSocket(socketFd)
-	if err != nil {
-		return 0, err
-	}
-
-	attempt := 1 // счетчик ретраев
-
-	for {
-		// Подключаемся
-		err = socket.Connect(opts.RedisIp, opts.RedisPort, socketFd)
-		if err != nil {
-			// Проверяем ошибки, при которых нет смысла делать ретраи
-			switch err {
-			case models.ErrSocketNoAccess, models.ErrSocketLocalPortInUse, models.ErrSocketNoLocalPorts, models.ErrAddrBadParams,
-			models.ErrConnectionInProcess, models.ErrSocketBadFD, models.ErrSignalInterruption:
-				return 0, err
-
-			}
-
-			if errors.Is(err, syscall.EAGAIN) {
-				// Делаем ретраи
-				if attempt < opts.RetryAmount {
-					attempt++
-					continue
-				} else {
-					return 0, models.ErrConnectionRetriesFailed
-				}
-			}
-		}
-
-		// Ждем результат
-		polling.Wait()
-
-		// Проверка события
-		err = polling.ProcessEvent(socketFd)
-		if err != nil {
-			// Проверяем ошибки, при которых нет смысла делать ретраи
-			if errors.Is(err, syscall.ECONNREFUSED) {
-				return 0, models.ErrConnectionRefused
-			}
-			if errors.Is(err, syscall.EHOSTUNREACH) {
-				return 0, models.ErrServerUnreachable
-			}
-			if errors.Is(err, syscall.ENETUNREACH) {
-				return 0, models.ErrNetUnreachable
-			}
-			if errors.Is(err, syscall.EACCES) {
-				return 0, models.ErrSocketNoAccess
-			}
-
-			// ErrSocketEvent, ErrSocketHUPEvent и ErrSocketRDHUPEvent точно не надо проверять?
-			
-			// Делаем ретраи
-			if attempt < opts.RetryAmount {
-				attempt++
-				continue
-			} else {
-				return 0, models.ErrConnectionRetriesFailed
-			}
-		}
-		break
-	}
-
-	// Ставим на отслеживание входящие события
-	err = polling.AddIncomeEventForSocket(socketFd)
-	if err != nil {
-		return 0, err
-	}
-
-	return socketFd, nil
+func NewConnection(opts *models.Options, e epoller) (*Connection, error) {
+	return &Connection{}, nil
 }
 
-// Reconnect переподключается к серверу
-func Reconnect(opts models.Options, oldSocketFd int) (int, error) {
-	// Закрываем старый сокет
-	syscall.Close(oldSocketFd)
+type epoller interface {
+	Add(models.PollingUnit) error
+	GetError() error
+}
+
+type Connection struct {
+	socketFd int
+	mu sync.Mutex
+	sendBuf *models.SendBuf
+	recvBuf *models.RecvBuf
+	poller epoller
+}
+
+func (c *Connection) GetSendBuf() *models.SendBuf {
+	return c.sendBuf
+}
+
+func (c *Connection) SendAndReceive(*models.SendBuf) (*models.RecvBuf, error) {
+	return c.recvBuf, nil
+}
+
+func (c *Connection) DrainRecvBuf(*models.RecvBuf) {
 	
-	// // Удаляем события закрытого сокета из списка отслеживания
-	// err := polling.DeleteEventsForSocket(oldSocketFd)
-	// if err != nil {
-	// 	return 0, err
-	// }
-
-	// Создаем и подключаем новый сокет
-	newSocketFd, err := New(opts)
-	if err != nil {
-		return 0, err
-	}
-
-	return newSocketFd, nil
 }
