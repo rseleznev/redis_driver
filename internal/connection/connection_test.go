3f1935be7334f3f90fcc90e69a1b5e872d707852
rseleznev <gradoozd@gmail.com>
2026-04-25 16:09:36 +0300

    Написал несколько тестов connection.poll

--- internal/connection/connection_test.go
+++ internal/connection/connection_test.go
@@ -11,21 +11,28 @@ import (
 )
 
 type mockPoller struct {
-	addFunc func(models.PollingUnit) error
+	done chan struct{}
+	
+	addFunc func(models.PollingUnit, <-chan struct{}) error
 	getErrorFunc func() error
 	deleteSocketFunc func(int)
 }
 
-func (mp mockPoller) Add(unit models.PollingUnit) error {
-	return mp.addFunc(unit)
+func (mp *mockPoller) Add(unit models.PollingUnit) error {
+	return mp.addFunc(unit, mp.done)
 }
 
-func (mp mockPoller) GetError() error {
+func (mp *mockPoller) GetError() error {
 	return mp.getErrorFunc()
 }
 
-func (mp mockPoller) DeleteSocketFromPolling(n int) {
+func (mp *mockPoller) DeleteSocketFromPolling(n int) {
+	mp.closeTestChan()
 	mp.deleteSocketFunc(n)
+}
+
+func (mp *mockPoller) closeTestChan() {
+	close(mp.done)
 }
 
 
@@ -80,9 +87,14 @@ func Test_connect(t *testing.T) {
 			},
 			expectedErr: nil,
 			mockPoll: mockPoller{
-				addFunc: func(pu models.PollingUnit) error {
+				addFunc: func(pu models.PollingUnit, done <-chan struct{}) error {
 					go func ()  {
-						pu.ResultChan <- nil
+						select {
+						case pu.ResultChan <- nil:
+
+						case <-done:
+
+						}
 					}()
 
 					return nil
@@ -105,7 +117,7 @@ func Test_connect(t *testing.T) {
 			},
 			expectedErr: models.ErrConnectionRetriesFailed,
 			mockPoll: mockPoller{
-				addFunc: func(pu models.PollingUnit) error {
+				addFunc: func(pu models.PollingUnit, done <-chan struct{}) error {
 					return models.ErrSocketAlreadyAdded
 				},
 			},
@@ -135,10 +147,108 @@ func Test_connect(t *testing.T) {
 	for _, tt := range testData {
 		t.Run(tt.name, func(t *testing.T) {
 			testConnection.opts = tt.opts
-			testConnection.poller = tt.mockPoll
+			testConnection.poller = &tt.mockPoll
 			testConnection.socket = tt.mockSock
 
 			err := testConnection.connect()
+			if err != tt.expectedErr {
+				t.Errorf("Ожидаемая ошибка %s, получено %s", tt.expectedErr, err)
+			}
+		})
+	}
+}
+
+func Test_poll(t *testing.T) {
+	testData := []struct{
+		name string
+		opts *models.Options
+		expectedErr error
+		mockPoll mockPoller
+		mockSock mockSocket
+	}{
+		{
+			name: "success",
+			opts: &models.Options{
+				PollingTimeout: time.Millisecond*500,
+			},
+			expectedErr: nil,
+			mockPoll: mockPoller{
+				addFunc: func(pu models.PollingUnit, done <-chan struct{}) error {
+					go func ()  {
+						select {
+						case pu.ResultChan <- nil:
+
+						case <-done:
+
+						}
+					}()
+
+					return nil
+				},
+			},
+			mockSock: mockSocket{
+				getSocketFdFunc: func() int {
+					return 2
+				},
+			},
+		},
+		{
+			name: "fail timeout poller.Add",
+			opts: &models.Options{
+				PollingTimeout: time.Millisecond*500,
+			},
+			expectedErr: models.ErrPollTimeout,
+			mockPoll: mockPoller{
+				addFunc: func(pu models.PollingUnit, done <-chan struct{}) error {
+					return models.ErrSocketAlreadyAdded
+				},
+			},
+			mockSock: mockSocket{
+				getSocketFdFunc: func() int {
+					return 2
+				},
+			},
+		},
+		{
+			name: "fail result timeout",
+			opts: &models.Options{
+				PollingTimeout: time.Millisecond*500,
+			},
+			expectedErr: models.ErrPollTimeout,
+			mockPoll: mockPoller{
+				done: make(chan struct{}),
+				addFunc: func(pu models.PollingUnit, done <-chan struct{}) error {
+					go func() {
+						time.Sleep(time.Second*1)
+						
+						select {
+						case pu.ResultChan <- nil:
+
+						case <-done:
+
+						}
+						
+					}()
+					
+					return nil
+				},
+				deleteSocketFunc: func(_ int) {},
+			},
+			mockSock: mockSocket{
+				getSocketFdFunc: func() int {
+					return 2
+				},
+			},
+		},
+	}
+
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			testConnection.opts = tt.opts
+			testConnection.poller = &tt.mockPoll
+			testConnection.socket = tt.mockSock
+
+			err := testConnection.poll("connect")
 			if err != tt.expectedErr {
 				t.Errorf("Ожидаемая ошибка %s, получено %s", tt.expectedErr, err)
 			}
