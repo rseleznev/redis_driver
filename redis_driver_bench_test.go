8d2d5b2da9156a9e276aa0693aa4198fe93007ab
rseleznev <gradoozd@gmail.com>
2026-05-04 13:26:56 +0300

    Добавил несколько бенч-тестов

--- redis_driver_bench_test.go
+++ redis_driver_bench_test.go
@@ -13,50 +13,69 @@ var (
 	longValue, twiceLongValue []byte
 	client *Client
 	err error
-	// testData []struct{
-	// 	key string
-	// 	value any
-	// } = []struct{
-	// 	key string
-	// 	value any
-	// }{
-	// 	{
-	// 		key: "test1",
-	// 		value: "value1",
-	// 	},
-	// 	{
-	// 		key: "test2",
-	// 		value: []byte{'V', 'A', 'L', 'U', 'E', '2'},
-	// 	},
-	// 	{
-	// 		key: "test3",
-	// 		value: longValue,
-	// 	},
-	// 	{
-	// 		key: "test4",
-	// 		value: twiceLongValue,
-	// 	},
-	// }
 )
 
-func BenchmarkSet(b *testing.B) {
+func BenchmarkHello(b *testing.B) {
 	ctx := context.Background()
 	client, err = NewClient(&options.Options{
 		RedisIp: [4]byte{127, 0, 0, 1},
 		RedisPort: 6379,
 		SendBufMinLen: 800,
 	})
-
 	if err != nil {
 		b.Fatal(err)
 	}
 
+	var result map[string]string
+
+	for b.Loop() {
+		r, err := client.Hello3(ctx)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		result = r
+	}
+
+	b.Log(result)
+}
+
+func BenchmarkSetShortValue(b *testing.B) {
+	ctx := context.Background()
+
+	for b.Loop() {
+		err = client.SetValueForKey(ctx, "test1", "value1", time.Minute)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	_ = err
+}
+
+func BenchmarkSetLongValue(b *testing.B) {
+	ctx := context.Background()
+
 	longValue, _ = os.ReadFile("./docs/testing/response.json")
+
+	for b.Loop() {
+		err = client.SetValueForKey(ctx, "test2", longValue, time.Minute)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	_ = err
+}
+
+func BenchmarkSetTwiceLongValue(b *testing.B) {
+	ctx := context.Background()
+
 	twiceLongValue = longValue
 	twiceLongValue = append(twiceLongValue, longValue...)
 
 	for b.Loop() {
-		err = client.SetValueForKey(ctx, "test", twiceLongValue, time.Minute)
+		err = client.SetValueForKey(ctx, "test3", twiceLongValue, time.Minute)
 		if err != nil {
 			b.Fatal(err)
 		}
